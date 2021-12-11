5d93a58a66517c27c308c19a2cbc16cfc458f671
madhurya <madhuryar017@gmail.com>
2021-12-11 22:24:44 +0530

    [madhurya] Adding Hashtable Assignment

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!"); //[3,7,9,11,55,67]
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+            Console.WriteLine("Sorted Elements are: ");
+            binarySearch.Display();
+            bool result = binarySearch.IfExists(63, binarySearch);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ HashTableAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTableAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
